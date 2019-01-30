284861e8a77cb2ed901054c92b1b1fa8664708a1
unknown <aldo1148@gmail.com>
2019-01-30 12:36:02 +0100

    Implemented enum Class:RequestStatus and VehicleCategory

---
+++ Source/src/it/carcheck/model/bean/enums/RequestStatus.java
@@ -0,0 +1,8 @@
+package it.carcheck.model.bean.enums;
+
+public class RequestStatus{
+    public static final int PROCESSING = 0;
+    public static final int REFUSED = 1;
+    public static final int APPOINTMENT = 2;
+    public static final int APPROVED = 3;
+}
