8950993c71ad50d6647e2dbf279a8060b204749c
Denny9700 <denny9700@gmail.com>
2019-02-07 09:57:36 +0100

    Added Carcheck Database Configuration

---
+++ Source/src/it/carcheck/database/CarcheckConfiguration.java
@@ -0,0 +1,9 @@
+package it.carcheck.database;
+
+import it.dsoft.fastcrud.DatabaseConfiguration;
+
+public class CarcheckConfiguration extends DatabaseConfiguration {
+	public CarcheckConfiguration() {
+		super("root", "root", "carcheck");
+	}
+}
