db24bb6b453baef2e0592e45c32d68a7dc25423f
Denny9700 <denny9700@gmail.com>
2019-02-09 19:11:38 +0100

    Added AddressAction

--- Source/src/it/carcheck/control/ActionFactory.java
+++ Source/src/it/carcheck/control/ActionFactory.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import it.carcheck.control.exception.ActionNotFoundException;
 import it.carcheck.control.interfaces.IAction;
 import it.carcheck.control.request.WorkshopLoginAction;
+import it.carcheck.control.service.AddressAction;
 import it.carcheck.control.service.CityAction;
 import it.carcheck.control.service.ProvinceAction;
 import it.carcheck.control.service.RegionAction;
@@ -24,6 +25,7 @@ public class ActionFactory {
 		actions.put("POST/region_service", new RegionAction());
 		actions.put("POST/province_service", new ProvinceAction());
 		actions.put("POST/city_service", new CityAction());
+		actions.put("POST/address_service", new AddressAction());
 	}
 	
 	public static IAction getAction(HttpServletRequest request) throws ActionNotFoundException{
