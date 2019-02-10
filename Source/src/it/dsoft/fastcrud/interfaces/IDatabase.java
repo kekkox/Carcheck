b0c64b484678e32a34425282ee4321b8f34cec51
Denny9700 <denny9700@gmail.com>
2019-02-10 15:43:38 +0100

    Fixed FastCrud Interface

--- Source/src/it/dsoft/fastcrud/interfaces/IDatabase.java
+++ Source/src/it/dsoft/fastcrud/interfaces/IDatabase.java
@@ -16,5 +16,5 @@ public interface IDatabase {
 	public <T> int create(T object) throws SQLException, ConfigurationException, ObjectMapException, TableNameException, Exception;
 	public <T> void update(T object) throws SQLException, ConfigurationException, ObjectMapException, TableNameException, Exception;
 	public <T> void delete(T object) throws SQLException, ConfigurationException, ObjectMapException, TableNameException, Exception;
-	public <T> LinkedHashSet<T> read(String query, Class<T> clazz) throws SQLException, ConfigurationException, Exception;
+	public <T> LinkedHashSet<T> read(Class<T> clazz, String query, Object...args) throws SQLException, ConfigurationException, Exception;
 }
