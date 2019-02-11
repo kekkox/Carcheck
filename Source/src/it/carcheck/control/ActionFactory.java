600fe1425fc2ac3653f3e626f51d7fd4f1b13a9d
Addx97 <aldo1148@gmail.com>
2019-02-11 11:29:26 +0100

    add new action to mapping

--- Source/src/it/carcheck/control/ActionFactory.java
+++ Source/src/it/carcheck/control/ActionFactory.java
@@ -7,12 +7,9 @@ import javax.servlet.http.HttpServletRequest;
 
 import it.carcheck.control.exception.ActionNotFoundException;
 import it.carcheck.control.interfaces.IAction;
-import it.carcheck.control.request.VehicleFindAction;
-import it.carcheck.control.request.WorkshopLoginAction;
-import it.carcheck.control.service.AddressAction;
-import it.carcheck.control.service.CityAction;
-import it.carcheck.control.service.ProvinceAction;
-import it.carcheck.control.service.RegionAction;
+import it.carcheck.control.request.*;
+import it.carcheck.control.service.*;
+
 
 public class ActionFactory {
 	
@@ -20,14 +17,20 @@ public class ActionFactory {
 		actions = new HashMap<String, IAction>();
 		
 		// Requests
+		actions.put("POST/signup", new WorkshopSignupAction());
 		actions.put("POST/login", new WorkshopLoginAction());
 		actions.put("POST/find", new VehicleFindAction());
+		actions.put("POST/loginAdmin", new AdminLoginAction());
+		actions.put("POST/signupAdmin", new AdminSignupAction());
+		actions.put("POST/logout", new LogoutAction());
 		
 		// Services
 		actions.put("POST/region_service", new RegionAction());
 		actions.put("POST/province_service", new ProvinceAction());
 		actions.put("POST/city_service", new CityAction());
 		actions.put("POST/address_service", new AddressAction());
+		
+	
 	}
 	
 	public static IAction getAction(HttpServletRequest request) throws ActionNotFoundException{
