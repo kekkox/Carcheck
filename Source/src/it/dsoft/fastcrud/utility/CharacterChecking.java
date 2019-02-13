24a4c07676dc636a613b266f634c4564c76ec1a3
Denny9700 <denny9700@gmail.com>
2019-02-13 16:17:08 +0100

    Small FastCrud Update

--- Source/src/it/dsoft/fastcrud/utility/CharacterChecking.java
+++ Source/src/it/dsoft/fastcrud/utility/CharacterChecking.java
@@ -2,6 +2,10 @@ package it.dsoft.fastcrud.utility;
 
 public class CharacterChecking {
 	public static String replaceInvalidCharacters(String str) {
-		return str.replace("'", "\'");
+		String newString = str;
+		newString = newString.replace("'", "\'");
+		newString = newString.replace("%", "\u0025"); //%
+		
+		return newString;
 	}
 }
