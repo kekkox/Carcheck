e38ba2f60bf4f089e5226600fb2ff744a10bc28d
Denny9700 <denny9700@gmail.com>
2019-02-14 11:48:12 +0100

    Added VehicleComplaint Manager and VehicleComplaintHistory Manager

---
+++ Source/src/it/carcheck/model/VehicleComplaintHistoryManager.java
@@ -0,0 +1,80 @@
+package it.carcheck.model;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import it.carcheck.database.CarcheckDatabase;
+import it.carcheck.model.bean.VehicleComplaintHistoryBean;
+import it.carcheck.model.interfaces.IVehicleComplaintHistory;
+
+public class VehicleComplaintHistoryManager implements IVehicleComplaintHistory {
+
+	public static VehicleComplaintHistoryManager getInstance() {
+		if(instance == null)
+			instance = new VehicleComplaintHistoryManager();
+		
+		return instance;
+	}
+	
+	private VehicleComplaintHistoryManager() {
+		this.database = CarcheckDatabase.getInstance();
+	}
+	
+	@Override
+	public void doSave(VehicleComplaintHistoryBean element) throws SQLException {
+		try {
+			this.database.update(element);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void doDelete(VehicleComplaintHistoryBean element) throws SQLException {
+		try {
+			this.database.delete(element);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+
+
+	@Override
+	public void doInsert(VehicleComplaintHistoryBean element) throws SQLException {
+		try {
+			this.database.create(element);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+
+
+	@Override
+	public ArrayList<VehicleComplaintHistoryBean> doFind(String query, Object... args) throws SQLException {
+		try {
+			return this.database.find(VehicleComplaintHistoryBean.class, query, args);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+
+
+
+	@Override
+	public Collection<VehicleComplaintHistoryBean> getVehicleComplaintsFromLicensePlate(String licensePlate) {
+		try {
+			return this.doFind("SELECT * FROM vehiclecomplaint_history WHERE licensePlate = ?", licensePlate);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private CarcheckDatabase database; 
+	private static VehicleComplaintHistoryManager instance;
+}
---
+++ Source/src/it/carcheck/model/VehicleComplaintManager.java
@@ -0,0 +1,81 @@
+package it.carcheck.model;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import it.carcheck.database.CarcheckDatabase;
+import it.carcheck.model.bean.VehicleComplaintBean;
+import it.carcheck.model.interfaces.IVehicleComplaint;
+
+public class VehicleComplaintManager implements IVehicleComplaint {
+
+	public static VehicleComplaintManager getInstance() {
+		if(instance == null)
+			instance = new VehicleComplaintManager();
+		
+		return instance;
+	}
+	
+	private VehicleComplaintManager() {
+		this.database = CarcheckDatabase.getInstance();
+	}
+	
+	@Override
+	public void doSave(VehicleComplaintBean element) throws SQLException {
+		try {
+			this.database.update(element);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void doDelete(VehicleComplaintBean element) throws SQLException {
+		try {
+			this.database.delete(element);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+
+
+	@Override
+	public void doInsert(VehicleComplaintBean element) throws SQLException {
+		try {
+			this.database.create(element);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+
+
+	@Override
+	public ArrayList<VehicleComplaintBean> doFind(String query, Object... args) throws SQLException {
+		try {
+			return this.database.find(VehicleComplaintBean.class, query, args);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+
+
+
+	@Override
+	public VehicleComplaintBean getVehicleComplaintFromLicensePlate(String licensePlate) {
+		try {
+			ArrayList<VehicleComplaintBean> vehicles = this.doFind("SELECT * FROM vehiclecomplaint WHERE licensePlate = ?", licensePlate);
+			if(vehicles != null && vehicles.size() > 0)
+				return vehicles.get(0);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	private CarcheckDatabase database; 
+	private static VehicleComplaintManager instance;
+}
