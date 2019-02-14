5b9d2eabfa1741c7b718c36b3294337ef2fb33e5
Denny9700 <denny9700@gmail.com>
2019-02-14 11:47:05 +0100

    Added new Beans

---
+++ Source/src/it/carcheck/model/bean/VehicleComplaintBean.java
@@ -0,0 +1,47 @@
+package it.carcheck.model.bean;
+
+import java.sql.Date;
+
+import it.dsoft.fastcrud.core.annotations.PrimaryKey;
+import it.dsoft.fastcrud.core.annotations.Table;
+import it.dsoft.fastcrud.core.enums.PrimaryKeyOption;
+
+public class VehicleComplaintBean {
+
+	@Table(name = "vehiclecomplaint")
+	public VehicleComplaintBean() {}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getLicensePlate() {
+		return licensePlate;
+	}
+	public void setLicensePlate(String licensePlate) {
+		this.licensePlate = licensePlate;
+	}
+	
+	public Date getComplaintDate() {
+		return complaintDate;
+	}
+	public void setComplaintDate(Date complaintDate) {
+		this.complaintDate = complaintDate;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@PrimaryKey(option = PrimaryKeyOption.Auto_Increment)
+	private int id;
+	private String licensePlate;
+	private Date complaintDate;
+	private String description;
+}
