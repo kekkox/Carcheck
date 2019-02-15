4c2e81720c7759b8e5947f5a80449ae9c913ce8d
kekkox <kekkox@live.it>
2019-02-15 10:45:52 +0100

    Implemented WorkshopAdhesionManager

---
+++ Source/src/it/carcheck/model/WorkshopAdhesionManager.java
@@ -0,0 +1,64 @@
+package it.carcheck.model;
+
+import java.util.Collection;
+
+import it.carcheck.database.CarcheckDatabase;
+import it.carcheck.model.bean.AdminBean;
+import it.carcheck.model.bean.WorkshopAdhesionBean;
+import it.carcheck.model.interfaces.IWorkshopAdhesion;
+
+public class WorkshopAdhesionManager implements IWorkshopAdhesion{
+
+	public static WorkshopAdhesionManager getInstance() {
+		if(instance == null)
+			instance = new WorkshopAdhesionManager();
+		
+		return instance;
+	}
+	
+	private WorkshopAdhesionManager() {
+		this.database = CarcheckDatabase.getInstance();
+	}
+	
+	@Override
+	public WorkshopAdhesionBean doRetrieveByWorkshopId(int id) {
+		final String query = "SELECT workshop.id AS workshopId, adhesionrequest.id AS adhesionRequestId, businessName, pIva, owner, description, telephone, email, status, address "
+				+ "FROM workshop, adhesionrequest "
+				+ "WHERE workshop.id = ? AND workshop.id = adhesionrequest.id";
+		try {
+			return database.find(WorkshopAdhesionBean.class, query , id).get(0);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	@Override
+	public WorkshopAdhesionBean doRetrieveByAdhesionId(int id) {
+		final String query = "SELECT workshop.id AS workshopId, adhesionrequest.id AS adhesionRequestId, businessName, pIva, owner, description, telephone, email, status, address "
+				+ "FROM workshop, adhesionrequest "
+				+ "WHERE adhesionrequest.id = ? AND workshop.id = adhesionrequest.id";
+		try {
+			return database.find(WorkshopAdhesionBean.class, query , id).get(0);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	@Override
+	public Collection<WorkshopAdhesionBean> doRetrieveAll(AdminBean admin) {
+		final String query = "SELECT workshop.id AS workshopId, adhesionrequest.id AS adhesionRequestId, businessName, pIva, owner, description, telephone, email, status, address "
+				+ "FROM workshop, adhesionrequest "
+				+ "WHERE admin.id = ? AND workshop.id = adhesionrequest.id";
+		try {
+			return database.find(WorkshopAdhesionBean.class, query , admin.getId());
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	private CarcheckDatabase database;
+	private static WorkshopAdhesionManager instance;
+}
