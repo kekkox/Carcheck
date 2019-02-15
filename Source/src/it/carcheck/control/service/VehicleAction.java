49d839083f225cc5f10ae02ec6dc5f57373ec21d
Denny9700 <denny9700@gmail.com>
2019-02-15 13:04:24 +0100

    Vehicle Action now can Return License Plates

--- Source/src/it/carcheck/control/service/VehicleAction.java
+++ Source/src/it/carcheck/control/service/VehicleAction.java
@@ -3,6 +3,9 @@ package it.carcheck.control.service;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,10 +14,12 @@ import com.google.gson.Gson;
 
 import it.carcheck.control.exception.ActionException;
 import it.carcheck.control.interfaces.IAction;
+import it.carcheck.model.VehicleInspectionManager;
 import it.carcheck.model.VehicleManager;
 import it.carcheck.model.bean.AdminBean;
 import it.carcheck.model.bean.JsonResponse;
 import it.carcheck.model.bean.VehicleBean;
+import it.carcheck.model.bean.VehicleInspectionBean;
 import it.carcheck.model.bean.WorkshopBean;
 import it.carcheck.model.bean.enums.JsonResponseStatus;
 
@@ -31,6 +36,7 @@ public class VehicleAction implements IAction {
 		//Operation type
 		// 1 -> Add vehicle
 		// 2 -> Remove vehicle
+		// 3 -> Find all by license plate
 		String operation_str = request.getParameter("operation");
 		if(operation_str == null) {
 			writer.println(gson.toJson(new JsonResponse(JsonResponseStatus.FAILED, "operation type is null")));
@@ -105,6 +111,37 @@ public class VehicleAction implements IAction {
 				} catch (SQLException e) {
 					writer.println(gson.toJson(new JsonResponse(JsonResponseStatus.FAILED, "unable to find or delete vehicle (DB Error)")));
 				}
+				 break;
+			 case 3:
+				 if(licensePlate == null) {
+					 writer.println(gson.toJson(new JsonResponse(JsonResponseStatus.FAILED, "license plate is null")));
+					 return "vehicle_service";
+				 }
+				 
+				 String workshopId_str = request.getParameter("workshop");
+				 if(workshopId_str == null) {
+					 writer.println(gson.toJson(new JsonResponse(JsonResponseStatus.FAILED, "workshop id is null")));
+					 return "vehicle_service";
+				 }
+				 
+				 int workshopId = Integer.parseInt(workshopId_str);
+				 VehicleInspectionManager inspectionManager = VehicleInspectionManager.getInstance();
+				 
+				 Collection<VehicleInspectionBean> inspections = inspectionManager.doRetrieveByWorkshopIdAndLicensePlate(workshopId, licensePlate);
+				 if(inspections.size() <= 0) {
+					 writer.println(gson.toJson(new JsonResponse(JsonResponseStatus.FAILED, "no vehicles inspection found")));
+					 return "vehicle_service";
+				 }
+				 
+				 ArrayList<String> licensePlates = new ArrayList<>();
+				 Iterator<VehicleInspectionBean> iterator = inspections.iterator();
+				 
+				 while(iterator.hasNext()) {
+					 String license = iterator.next().getVehicle();
+					 licensePlates.add(license);
+				 }
+				 
+				 writer.println(gson.toJson(new JsonResponse(JsonResponseStatus.OK, "", licensePlates)));
 				 break;
 			 default:
 				 writer.println(gson.toJson(new JsonResponse(JsonResponseStatus.FAILED, "invalid operation type")));
