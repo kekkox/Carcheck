4959cb428e6d3bd2bc75aa9c1c7100b5fd8e964e
Addx97 <aldo1148@gmail.com>
2019-02-16 16:06:19 +0100

    Update Parameter Workshop Insert Action

--- Source/src/it/carcheck/control/request/WorkshopViewInspectionAction.java
+++ Source/src/it/carcheck/control/request/WorkshopViewInspectionAction.java
@@ -16,24 +16,28 @@ public class WorkshopViewInspectionAction implements IAction {
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws ActionException {
 		int inspectionKey = Integer.parseInt(request.getParameter("inspectionKey"));
 		WorkshopBean workshop = (WorkshopBean) request.getSession().getAttribute("user");
-		
+		response.setHeader(IAction.HEADER_NAME, IAction.FORWARD_RESPONSE);
 		VehicleInspectionManager inspection = VehicleInspectionManager.getInstance();
 		try{
-		VehicleInspectionBean inspectionBean = (VehicleInspectionBean)inspection.doRetrieveByKey(workshop, inspectionKey);
+		VehicleInspectionBean inspectionBean = inspection.doRetrieveByKey(workshop, inspectionKey);
 		request.setAttribute("property", "readonly");
-		request.setAttribute("class", "classname=active");
+		request.setAttribute("classname", "class=active");
 		request.setAttribute("title", "Visualizza Revisione");
-		request.setAttribute("uploadIsVisible", "");
+		request.setAttribute("upload", "fullView");
+		
+		request.setAttribute("inspectionsView", inspectionBean);
+		/*
 		request.setAttribute("licenseplate", inspectionBean.getVehicle());
 		request.setAttribute("inspectionDate", inspectionBean.getInspectionDate());
 		request.setAttribute("km", inspectionBean.getKm());
 		request.setAttribute("approved", inspectionBean.isResult());
+		*/
 		}
 
 		catch (SQLException e) {
 			throw new ActionException();
 		}
-		return null;
+		return "inspectionView";
 
 	}
 }
