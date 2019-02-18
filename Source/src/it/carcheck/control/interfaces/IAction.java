2f6f3b5aa81436feffe8f687d0b0538305c225b1
Addx97 <aldo1148@gmail.com>
2019-02-18 10:36:20 +0100

    Update Action Exception

--- Source/src/it/carcheck/control/interfaces/IAction.java
+++ Source/src/it/carcheck/control/interfaces/IAction.java
@@ -2,14 +2,16 @@ package it.carcheck.control.interfaces;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.text.ParseException;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import it.carcheck.control.exception.ActionException;
 
 public interface IAction {
-	public String execute(HttpServletRequest request, HttpServletResponse response) throws ActionException, IOException;
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws ActionException, IOException, ServletException, ParseException;
 	
 	public static final String HEADER_NAME = "RESPONSE_TYPE";
 	public static final String JSON_RESPONSE = "JSON";
